37215cc70bb8009f5c3d2e99a8bba772b9094a1c
Rakshitha <27rakshitha@gmail.com>
2024-05-16 15:52:17 +0530

    Using setState() in React Hook

--- routing-demo/src/component/hookscomponent.js
+++ routing-demo/src/component/hookscomponent.js
@@ -4,10 +4,17 @@ import React, {useState} from "react";
 
 function HooksComponent () {
     const [title] = useState('Welcome to Edureka!!');
+
+    //setting state
+    const [count, setCount] = useState(0)
     return (
         <div>
             <center>
                 <h2>{title}</h2>
+                <p>{count}</p>
+                <button onClick={() => {setCount(count+1)} }>
+                    Enroll
+                </button>
             </center>
         </div>
     )
