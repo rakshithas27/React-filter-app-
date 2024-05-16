6449e1275e9cdd7dfa3c03e3a988f776e76e869e
Rakshitha <27rakshitha@gmail.com>
2024-05-16 18:16:20 +0530

    Tried useEffect() in React Hooks

--- routing-demo/src/component/hookscomponent.js
+++ routing-demo/src/component/hookscomponent.js
@@ -1,12 +1,20 @@
-//Defining state inside function component
 
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+
 
 function HooksComponent () {
     const [title] = useState('Welcome to Edureka!!');
-
-    //setting state
     const [count, setCount] = useState(0)
+    const [topics, setTopics] = useState([])
+
+    useEffect(() => {
+        const fetchTpoics = async () => {
+            const data = ['Component', 'Lifecycle', 'States', 'Props', 'Redux', 'Hooks', 'Routing']
+            setTopics(data)
+        }
+        fetchTpoics();
+    }, []);
+
     return (
         <div>
             <center>
@@ -15,9 +23,19 @@ function HooksComponent () {
                 <button onClick={() => {setCount(count+1)} }>
                     Enroll
                 </button>
+                <h3>Important Topics That we cover:</h3>
+                <div>
+                    {topics.map((topic,index) => {
+                        return <li key = {index}>{topic}</li>       
+                    })}
+                </div>  
             </center>
         </div>
     )
 }
 
 export default HooksComponent;
+
+
+
+
